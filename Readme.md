5021af5a0239041f5bec28f1d2bf6f8f1991ca21
n.simchuk <n.simchuk@tinkoff.ru>
2023-12-10 18:32:09 +0300

    chore: added palette generation

--- Readme.md
+++ Readme.md
@@ -91,4 +91,4 @@ Each color may be used in class. For example:
 </div>
 ```
 
-![minimalist](./release/colorsMinimalist.svg)
+![Minimalist palettes](release/colorsMinimalist.svg)
---
+++ release/colors.mjs
@@ -0,0 +1,77 @@
+import { readFile, writeFile } from "node:fs/promises";
+import * as fs from "fs";
+import {special} from '../dist/main.cjs.js'
+const palettes = special.minimalist;
+
+import { html } from "satori-html";
+import satori from "satori";
+
+function minimalist(palette, name) {
+    return `\t<div>
+        ${name}
+        <div class='back' style='background: ${palette.back}; border: 2px solid ${palette.fore}'>
+            <div class='color' style='background: ${palette.main}'></div>
+            <div class='color' style='background: ${palette.sub}'></div>
+            <div class='color' style='background: ${palette.fore}'></div>
+        </div>
+    </div>`
+}
+
+let data = fs.readFileSync('./Readme.md').toString();
+
+async function processMinimalist(data) {
+    const template = html(`
+<div class="minimalist">
+${Object.keys(palettes).map(key=>{
+    if (key === '_template_') return '';
+    const palette = palettes[key].theme.colors;
+    return minimalist(palette, key.at(0).toLocaleUpperCase() + key.slice(1))
+}).join('\n')}
+</div>
+<style>
+    .minimalist {
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        color: #888;
+    }
+    .minimalist>div{
+        display: flex;
+        flex-direction: column;
+        margin-left: 8px;
+        margin-top: 16px;
+    }
+    .back {
+        display: flex;
+        flex-direction: row;
+        padding: 8px;
+        padding-left: 0;
+        border-radius: 100px;
+    }
+    .color {
+        display: flex;
+        width: 32px;
+        height: 32px;
+        margin-left: 8px;
+        border-radius: 100px;
+    }
+</style>
+`);
+    const svg = await satori(template, {
+        width: 500,
+        height: Math.floor(Object.keys(palettes).length / 3) * (16 + 32 + 8 * 3 + 4),
+        fonts: [
+        {
+            name: "VictorMono",
+            data: await readFile("./release/Roboto-Regular.ttf"),
+            weight: 500,
+            style: "normal",
+        },
+        ],
+    });
+    fs.writeFileSync('./release/colorsMinimalist.svg', svg)
+}
+
+let res = processMinimalist(data);
