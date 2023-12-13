d2e1b26d1238d92f2458cab2925a9494b033f9ce
n.simchuk <n.simchuk@tinkoff.ru>
2023-12-13 15:54:18 +0300

    chore: added more colors

--- src/special/minimalist.ts
+++ src/special/minimalist.ts
@@ -178,6 +178,142 @@ export const minimalist = {
         extra: '#64CCC5',
         error: '#176B87',
     }),
+    twilight: unpack({
+        back: "#232136",
+        fore: "#e0def4",
+        main: "#b9bfca",
+        sub: "#6e6a86",
+        extra: "#988ba2",
+        error: "#ff5370"
+    }),
+    retrowave: unpack({
+        back: "#2b213a",
+        fore: "#ffffff",
+        main: "#ff7edb",
+        sub: "#45a29e",
+        extra: "#66fcf1",
+        error: "#fc6677"
+    }),
+    zen: unpack({
+        back: "#282c34",
+        fore: "#abb2bf",
+        main: "#98c379",
+        sub: "#56b6c2",
+        extra: "#c678dd",
+        error: "#e06c75"
+    }),
+    pixel: unpack({
+        back: "#1d1d1d",
+        fore: "#ffffff",
+        main: "#fc3a95",
+        sub: "#19b2ff",
+        extra: "#ffba4b",
+        error: "#ff5555"
+    }),
+    sunset: unpack({
+        back: "#343d46",
+        fore: "#dfe1e8",
+        main: "#ec6053",
+        sub: "#f0c674",
+        extra: "#6699cc",
+        error: "#cc6666"
+    }),
+    neon: unpack({
+        back: "#212121",
+        fore: "#b4e7f8",
+        main: "#4bf3ff",
+        sub: "#edbfbf",
+        extra: "#8ae1fc",
+        error: "#ff89bd"
+    }),
+    mystic: unpack({
+        back: "#2f2057",
+        fore: "#edecee",
+        main: "#506c90",
+        sub: "#a5a38a",
+        extra: "#947e9c",
+        error: "#ff7b93"
+    }),
+    urban: unpack({
+        back: "#47494e",
+        fore: "#d9dbe0",
+        main: "#ff4088",
+        sub: "#3fdcee",
+        extra: "#a9e34b",
+        error: "#ff6347"
+    }),
+    oceanic: unpack({
+        back: "#0f2027",
+        fore: "#8fc9ff",
+        main: "#3aa0d8",
+        sub: "#66ffee",
+        extra: "#ffee66",
+        error: "#ff3333"
+    }),
+    sakura: unpack({
+        back: "#feedf3",
+        fore: "#5c5c61",
+        main: "#f7aef8",
+        sub: "#a1eafb",
+        extra: "#f6c7b6",
+        error: "#ff79b0"
+    }),
+    terra: unpack({
+        back: "#262221",
+        fore: "#e2c5b7",
+        main: "#a25922",
+        sub: "#856c57",
+        extra: "#d9a775",
+        error: "#e05252"
+    }),
+    vinyl: unpack({
+        back: "#1e1e1e",
+        fore: "#eeeeee",
+        main: "#ffcd60",
+        sub: "#bd93f9",
+        extra: "#ff5555",
+        error: "#ff79c6"
+    }),
+    cyber: unpack({
+        back: "#17141f",
+        fore: "#f3f3f3",
+        main: "#00ffff",
+        sub: "#ff00ff",
+        extra: "#ff006e",
+        error: "#ff5555"
+    }),
+    marble: unpack({
+        back: "#eceae8",
+        fore: "#5e5c64",
+        main: "#b4ada3",
+        sub: "#a09d96",
+        extra: "#6e6865",
+        error: "#d65f5f"
+    }),
+    solar: unpack({
+        back: "#202020",
+        fore: "#fdf6e3",
+        main: "#b58900",
+        sub: "#586e75",
+        extra: "#93a1a1",
+        error: "#dc322f"
+    }),
+    canopy: unpack({
+        back: "#fdf6e3",
+        fore: "#586e75",
+        main: "#859900",
+        sub: "#2aa198",
+        extra: "#93a1a1",
+        error: "#dc322f"
+    }),
+    dune: unpack({
+        back: "#322d00",
+        fore: "#e1d9a3",
+        main: "#d9b775",
+        sub: "#a88c56",
+        extra: "#796857",
+        error: "#ff6f6b"
+    }),
     _template_: unpack({
         back: '#',
         fore: '#',
